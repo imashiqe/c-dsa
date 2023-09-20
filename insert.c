3fef710ded84b5b50409f928fccec22732610014
Ashiqur Rahman <imashiqe@gmail.com>
2023-09-21 00:05:21 +0600

    inserting

--- insert.c
+++ insert.c
@@ -1,11 +1,11 @@
-// variable declaration  and  taking  elements  in  array
+
 
 #include <stdio.h>
 
 int main()
 {
-    int  array[100],position, i,n,value;
-
+    int  array[0],position, i,n,value;
+// variable declaration  and  taking  elements  in  array
     printf("Enter number of elements in  array\n");
     scanf("%d", &n);
 
@@ -14,8 +14,31 @@ int main()
     for(i = 1; i<= n; i++){
         scanf("%d" , &array[i]);
     }
-   
+//    Step-2: Insert new Value in Array
+     printf("Enter the location where you wish to insert an elements\n");
 
-  
+    scanf("%d" , &position);
+
+    printf("Enter the value to insert\n");
+
+    scanf("%d", &value);
+
+    for(i=n; i >= position; i--)
+    {
+        array[i+1] = array[i];
+
+    }
+    array[position] = value;
+
+    n=n+1;
+// Step-3: Show the Updated Array
+    printf("Resultant array is \n");
+
+    for(i =1; i<= n; i++)
+     {
+        printf("%d\n", array[i]);
+        return 0;
+     }
+
     return 0;
 }
---
+++ insertone.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+// insert new value in array
+int main()
+{
+    printf("Enter the location where you wish to insert an elements\n");
+
+    return 0;
+}
