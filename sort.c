34ffd33db059bdf0055d9da4a4a2742dcc1d93c9
Ashiqur Rahman <imashiqe@gmail.com>
2023-09-26 10:46:54 +0600

    linear search

---
+++ sort.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main()
+{
+    int i, n, j , index_min , temp , A[0];
+
+    for(i=0; i< n-1; i++)
+    {
+        index_min =i;
+
+        for(j =i+1; j <n; j++)
+        {
+        if(j = i+1, j <n, j++)
+         {
+            index_min = j;
+         };
+        };
+    };
+
+    if(index_min != i)
+    {
+        A[i] , A[index_min]
+          temp = A[i];
+          A[i] = A[index_min];
+          A[index_min] = temp;
+
+    };
+
+  
+
+    
+    return 0;
+}
